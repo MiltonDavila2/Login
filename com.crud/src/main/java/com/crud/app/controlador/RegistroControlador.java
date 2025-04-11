ba77762ab14d0272d56b9a97f8bcdcff4ab34ca5
MiltonDavila2 <miltondaviladt@gmail.com>
2025-04-10 19:47:56 -0500

    Terminado
    El programa esta terminado

--- com.crud/src/main/java/com/crud/app/controlador/RegistroControlador.java
+++ com.crud/src/main/java/com/crud/app/controlador/RegistroControlador.java
@@ -2,6 +2,9 @@ package com.crud.app.controlador;
 
 import com.crud.app.servicio.TrabajadorServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +22,16 @@ public class RegistroControlador {
 
     @GetMapping("/")
     public String VerPaginaDeInicio(Model modelo){
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String nombreUsuario = "Invitado";
+        if (auth != null && auth.getPrincipal() instanceof UserDetails) {
+            nombreUsuario = ((UserDetails) auth.getPrincipal()).getUsername();
+        }
+
+        modelo.addAttribute("nombreUsuario", nombreUsuario);
         modelo.addAttribute("trabajadores", servicio.ListarTrabajadores());
+
         return "trabajadores";
     }
 }
